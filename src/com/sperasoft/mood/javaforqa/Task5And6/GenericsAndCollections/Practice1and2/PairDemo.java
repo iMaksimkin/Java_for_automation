464f3f85130deefd1caef6aa7fb2e6104f77f394
iMaksimkin <int-8@yandex.ru>
2019-06-25 17:54:45 +0400

    Merge branch 'dev' of https://github.com/iMaksimkin/Java_for_automation into dev
    # Conflicts:
    #	src/com/sperasoft/mood/javaforqa/Task3/inheritance/Circle.java
    #	src/com/sperasoft/mood/javaforqa/Task3/inheritance/CircleCylinderDemo.java
    #	src/com/sperasoft/mood/javaforqa/Task3/inheritance/CircleShape.java
    #	src/com/sperasoft/mood/javaforqa/Task3/inheritance/Cylinder.java
    #	src/com/sperasoft/mood/javaforqa/Task3/inheritance/Math1.java
    #	src/com/sperasoft/mood/javaforqa/Task3/inheritance/MathDemo.java
    #	src/com/sperasoft/mood/javaforqa/Task3/inheritance/Rectangle.java
    #	src/com/sperasoft/mood/javaforqa/Task3/inheritance/Shape.java
    #	src/com/sperasoft/mood/javaforqa/Task3/inheritance/ShapeDemo.java
    #	src/com/sperasoft/mood/javaforqa/Task3/inheritance/Square.java
    #	src/com/sperasoft/mood/javaforqa/inheritance/Circle.java
    #	src/com/sperasoft/mood/javaforqa/inheritance/CircleCylinderDemo.java
    #	src/com/sperasoft/mood/javaforqa/inheritance/Cylinder.java
    #	src/com/sperasoft/mood/javaforqa/inheritance/Math1.java
    #	src/com/sperasoft/mood/javaforqa/inheritance/MathDemo.java
    #	src/com/sperasoft/mood/javaforqa/inheritance/Rectangle.java
    #	src/com/sperasoft/mood/javaforqa/inheritance/Shape.java
    #	src/com/sperasoft/mood/javaforqa/inheritance/Square.java
    #	src/com/sperasoft/mood/javaforqa/task3And4/inheritance/Circle.java
    #	src/com/sperasoft/mood/javaforqa/task3And4/inheritance/CircleCylinderDemo.java
    #	src/com/sperasoft/mood/javaforqa/task3And4/inheritance/CustomMathUtil.java
    #	src/com/sperasoft/mood/javaforqa/task3And4/inheritance/Cylinder.java
    #	src/com/sperasoft/mood/javaforqa/task3And4/inheritance/MathDemo.java
    #	src/com/sperasoft/mood/javaforqa/task3And4/inheritance/RectangleShape.java
    #	src/com/sperasoft/mood/javaforqa/task3And4/inheritance/Shape.java
    #	src/com/sperasoft/mood/javaforqa/task3And4/inheritance/SquareShape.java

--- src/com/sperasoft/mood/javaforqa/Task5And6/GenericsAndCollections/Practice1and2/PairDemo.java
+++ src/com/sperasoft/mood/javaforqa/Task5And6/GenericsAndCollections/Practice1and2/PairDemo.java
@@ -20,25 +20,18 @@ public class PairDemo {
 
         System.out.println(name + " " + rectangleShape);
 
-        Pair<String, String> secondObjectsPair = new Pair<>();
+
 
         String str1 = new String("square");
         String str2 = new String("square");
         String str3 = new String("rectangle");
 
-        System.out.println("First string" + str1);
-        System.out.println("2nd string" + str2);
-        System.out.println("3rd string" + str3);
+        System.out.println("First string " + str1);
+        System.out.println("2nd string " + str2);
+        System.out.println("3rd string " + str3);
 
-        System.out.println("Comparing 1st string and 2nd " + secondObjectsPair.compare(str1, str2));
-        System.out.println("Comparing 1st string and 3rd " + secondObjectsPair.compare(str1, str3));
-   //     Pair<Circle, Circle> thirdObjectsPair = new Pair<>();
+        System.out.println("Comparing 1st string and 2nd " + Pair.compare(str1, str2));
+        System.out.println("Comparing 1st string and 3rd " + Pair.compare(str1, str3));
 
-//        Shape rectangle = new RectangleShape("BLUE", true, 4, 4);
-//        Shape square = new SquareShape("Blue", true, 4);
-//        System.out.println("Comparing 1st string and 2nd " + thirdObjectsPair.compare(square, rectangle));
-//    }
-//        Circle circle1 = new Circle(1.2, Circle.Colour.GREEN);
-//        Circle circle2 = new Circle(1.2, Circle.Colour.GREEN);
-//        System.out.println("Comparing 1st string and 2nd " + thirdObjectsPair.compare(circle1, circle2);
-}}
+    }
+}
