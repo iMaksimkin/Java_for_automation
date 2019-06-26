c336b318c7be7e1be9c7194ffbcda214441480c6
iMaksimkin <int-8@yandex.ru>
2019-06-26 15:31:04 +0400

    Turtle complete

--- src/com/sperasoft/mood/javaforqa/task2/TurtleGraphics/TurtleGraphics.java
+++ src/com/sperasoft/mood/javaforqa/task2/TurtleGraphics/TurtleGraphics.java
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 package com.sperasoft.mood.javaforqa.task2.TurtleGraphics;
 
 
@@ -50,10 +49,8 @@ public class TurtleGraphics {
 
 
     public TurtleGraphics(int width, int height) {
-        if (width > 0 && height > 0) this.board = new char[width][height];
 
-        else System.out.println("incoorect size! ");
-        clearBoard();
+
     }
 
     public TurtleGraphics(Position penPosition, char cellChar, char colouredCellChar, char penChar, char[][] board) {
@@ -67,12 +64,19 @@ public class TurtleGraphics {
 
     public TurtleGraphics(int width, int height, int x, int y) {
 
-        //  this.penPosition = new Position(x, y);
-        //   this.board = new char[width][height];
-        if (width > 0 && height > 0) this.board = new char[width + 1][height];
-        else System.out.println("incoorect size! ");
-        if (x >= 0 && y >= 0 && width-x>0 && height-y>0) this.penPosition = new Position(x, y);
-        else System.out.println("incoorect position! ");
+
+        if (width > 0 && height > 0) {
+            this.board = new char[width + 1][height];
+        } else {
+            this.board = new char[6][5];
+            System.out.println("incoorect size! ");
+        }
+        if (x >= 0 && y >= 0 && x <= width && y <= height) {
+            this.penPosition = new Position(x, y);
+        } else {
+            this.penPosition = new Position();
+            System.out.println("incoorect position! ");
+        }
         clearBoard();
         board[this.penPosition.getX()][this.penPosition.getY()] = penChar;
     }
@@ -91,246 +95,7 @@ public class TurtleGraphics {
 
 
     public void movePenUp(int length) {
-
-        if (length+penPosition.getY()>board[0].length)
-        {
-           penPosition.setY(length-board[0].length);
-            System.out.println("The value is not valid. Try Again!");
-        }
-
-else
-            for (int i = 0; i < length; i++) {
-
-
-                board[penPosition.getX()][penPosition.getY()] = colouredCellChar;
-
-                penPosition.setY((penPosition.getY() - 1));
-                board[this.penPosition.getX()][this.penPosition.getY()] = penChar;
-
-            }
-
-        //  setPenPosition(new Position(penPosition.getX(), penPosition.getY()));
-    }
-
-    public void movePenDown(int length) {
-
-        if (length+penPosition.getY()>=board[0].length)
-        {
-            penPosition.setY(length-board[0].length);
-            System.out.println("The value is not valid. Try Again!");
-
-        } else
-            for (int i = 0; i < length; i++) {
-                board[penPosition.getX()][penPosition.getY()] = colouredCellChar;
-
-                penPosition.setY(penPosition.getY() + 1);
-                board[this.penPosition.getX()][this.penPosition.getY()] = penChar;
-            }
-
-
-    }
-
-    public void movePenLeft(int length) {
-        if (length+penPosition.getX()<board[0].length)
-        {
-            penPosition.setX(length-board[0].length);
-            System.out.println("The value is not valid. Try Again!");
-
-        } else
-            for (int i = 0; i < length; i++) {
-
-                board[penPosition.getX()][penPosition.getY()] = colouredCellChar;
-
-                penPosition.setX(penPosition.getX() - 1);
-                board[this.penPosition.getX()][this.penPosition.getY()] = penChar;
-            }
-
-
-    }
-
-    public void movePenRight(int length) {
-
-        if (length+penPosition.getX()>=board[0].length)
-        {
-            penPosition.setX(length-board[0].length);
-            System.out.println("The value is not valid. Try Again!");
-
-        } else
-            for (int i = 0; i < length; i++) {
-
-                board[penPosition.getX()][penPosition.getY()] = colouredCellChar;
-
-                penPosition.setX(penPosition.getX() + 1);
-                board[this.penPosition.getX()][this.penPosition.getY()] = penChar;
-
-
-            }
-
-
-    }
-
-
-    public void clearBoard() {
-        for (int i = 0; i < board.length - 1; i++) {
-            for (int j = 0; j < board[i].length; j++) {
-                board[i][j] = cellChar;
-            }
-        }
-
-
-    }
-
-
-//    public void setPenPosition(Position penPosition) {
-//
-//        this.penPosition = penPosition;
-//        board[this.penPosition.getX()][this.penPosition.getY()] = penChar;
-//
-//    }
-}
-
-class Position {    //inner class  /// maybe static?
-    //  @Getter
-    //   @Setter
-    private int x;
-    //   @Getter
-    //   @Setter
-
-    public void setX(int x) {
-        this.x = x;
-    }
-
-    //   @Getter
-    //   @Setter
-    public void setY(int y) {
-        this.y = y;
-    }
-
-    private int y;
-
-    //   @Getter
-    //   @Setter
-    Position() {
-
-        this.x = 0;
-        this.y = 0;
-    }
-
-
-    Position(int x, int y) {
-        if (x <= 0) this.x = 0;
-        else
-            this.x = x;
-        if (y <= 0) this.y = 0;
-        else
-            this.y = y;
-    }
-
-    public int getX() {
-        return x;
-    }
-
-    public int getY() {
-        return y;
-    }
-
-
-}
-=======
-package com.sperasoft.mood.javaforqa.task2.TurtleGraphics;
-
-
-import jdk.nashorn.internal.objects.annotations.Getter;
-import jdk.nashorn.internal.objects.annotations.Setter;
-
-//TODO penPosition and array bounds
-public class TurtleGraphics {
-    public enum Direction {
-        UP("u"),
-        DOWN("d"),
-        RIGHT("r"),
-        LEFT("l");
-
-        private String dir;
-
-        Direction(String dir) {
-            this.dir = dir;
-        }
-
-        public String getDir() {
-            return dir;
-        }
-
-
-    }
-
-    // @Getter
-    //   @Setter
-    private Position penPosition;
-
-    //  @Getter
-    //   @Setter
-    private char cellChar = '.';
-    //   @Getter
-    //   @Setter
-    private char colouredCellChar = 'o';
-
-    // @Getter
-    //   @Setter
-    private char penChar = 'x';
-    //  @Getter
-    //   @Setter
-    private char[][] board;
-
-
-    public TurtleGraphics() {
-    }
-
-
-    public TurtleGraphics(int width, int height) {
-        if (width > 0 && height > 0) this.board = new char[width][height];
-
-        else System.out.println("incoorect size! ");
-        clearBoard();
-    }
-
-    public TurtleGraphics(Position penPosition, char cellChar, char colouredCellChar, char penChar, char[][] board) {
-        this.penPosition = penPosition;
-        this.cellChar = cellChar;
-        this.colouredCellChar = colouredCellChar;
-        this.penChar = penChar;
-        this.board = board;
-
-    }
-
-    public TurtleGraphics(int width, int height, int x, int y) {
-
-        //  this.penPosition = new Position(x, y);
-        //   this.board = new char[width][height];
-        if (width > 0 && height > 0) this.board = new char[width + 1][height];
-        else System.out.println("incoorect size! ");
-        if (x >= 0 && y >= 0 ) this.penPosition = new Position(x, y);
-        else System.out.println("incoorect position! ");
-        clearBoard();
-        board[this.penPosition.getX()][this.penPosition.getY()] = penChar;
-    }
-
-
-    public void showBoard() {
-        for (int j = 0; j < board[0].length; j++) {
-            for (int i = 0; i < board.length; i++) {
-                System.out.print(board[i][j] + " ");
-            }
-            System.out.println();
-        }
-
-
-    }
-
-
-    public void movePenUp(int length) {
-        if (penPosition.getY()-length<0)
-        {
+        if (penPosition.getY() - length < 0) {
             penPosition.setY(penPosition.getY());
             System.out.println("The value is not valid. Try Again!");
 
@@ -352,9 +117,8 @@ public class TurtleGraphics {
 
     public void movePenDown(int length) {
 
-     if   (length+penPosition.getY()>=board[0].length+1)
-        {
-            penPosition.setY(length+board[0].length);
+        if (length + penPosition.getY() >= board[0].length) {
+            penPosition.setY(board[0].length - length);
             System.out.println("The value is not valid. Try Again!");
 
         } else
@@ -369,8 +133,7 @@ public class TurtleGraphics {
     }
 
     public void movePenLeft(int length) {
-        if (penPosition.getX()-length<=0)
-        {
+        if (penPosition.getX() - length < 0) {
             penPosition.setX(penPosition.getX());
             System.out.println("The value is not valid. Try Again!");
 
@@ -388,9 +151,8 @@ public class TurtleGraphics {
 
     public void movePenRight(int length) {
 
-        if (length+penPosition.getX()>=board[0].length+1)
-        {
-            penPosition.setX(length+board[0].length);
+        if (length + penPosition.getX() >= board[0].length) {
+            penPosition.setX(board[0].length - length);
             System.out.println("The value is not valid. Try Again!");
 
         } else
@@ -427,7 +189,7 @@ public class TurtleGraphics {
 //    }
 }
 
-class Position {    //inner class  /// maybe static?
+class Position {    //inner class
     //  @Getter
     //   @Setter
     private int x;
@@ -474,4 +236,4 @@ class Position {    //inner class  /// maybe static?
 
 
 }
->>>>>>> 8ca6767a27a070bf3c7f58104491afd5475db1a7
+
--- src/com/sperasoft/mood/javaforqa/task2/TurtleGraphics/TurtleGraphicsDemo.java
+++ src/com/sperasoft/mood/javaforqa/task2/TurtleGraphics/TurtleGraphicsDemo.java
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 package com.sperasoft.mood.javaforqa.task2.TurtleGraphics;
 
 
@@ -23,7 +22,7 @@ public class TurtleGraphicsDemo {
         System.out.println("x: ");
         int y = in.nextInt();
         System.out.println("y: ");
-        int x= in.nextInt();
+        int x = in.nextInt();
 
         TurtleGraphics turtleGraphics = new TurtleGraphics(width, height, x, y);
 
@@ -34,90 +33,30 @@ public class TurtleGraphicsDemo {
         System.out.println("please write the command");
 
         String line = sc.nextLine();
-while(true){
-     String[] dir = line.trim().split(" ");
+        while (true) {
+            String[] dir = line.trim().split(" ");
 
-          switch (dir[0]) {
-            case "u":
-                turtleGraphics.movePenUp(Integer.parseInt(dir[1]));
-                break;
-            case "d":
-                turtleGraphics.movePenDown(Integer.parseInt(dir[1]));
-                break;
-            case "l":
-                turtleGraphics.movePenLeft(Integer.parseInt(dir[1]));
-                break;
-            case "r":
-                turtleGraphics.movePenRight(Integer.parseInt(dir[1]));
-                break;
-              case "c":
-                  turtleGraphics.clearBoard();
-                  break;
+            switch (dir[0]) {
+                case "u":
+                    turtleGraphics.movePenUp(Integer.parseInt(dir[1]));
+                    break;
+                case "d":
+                    turtleGraphics.movePenDown(Integer.parseInt(dir[1]));
+                    break;
+                case "l":
+                    turtleGraphics.movePenLeft(Integer.parseInt(dir[1]));
+                    break;
+                case "r":
+                    turtleGraphics.movePenRight(Integer.parseInt(dir[1]));
+                    break;
+                case "c":
+                    turtleGraphics.clearBoard();
+                    break;
+            }
+            turtleGraphics.showBoard();
+            System.out.print("Print command: ");
+            line = sc.nextLine();
         }
-        turtleGraphics.showBoard();
-        System.out.print("Print command: ");
-        line = sc.nextLine();
     }
-}}
-=======
-package com.sperasoft.mood.javaforqa.task2.TurtleGraphics;
+}
 
-
-import java.util.Scanner;
-
-public class TurtleGraphicsDemo {
-    public static void main(String[] args) {
-
-
-        Scanner sc = new Scanner(System.in);
-        Scanner in = new Scanner(System.in);
-
-        System.out.println("Input the size of the board");
-        System.out.println("width: ");
-        int width = in.nextInt();
-
-        System.out.println("height: ");
-        int height = in.nextInt();
-        System.out.println();
-
-        System.out.println("start pen position: ");
-        System.out.println("x: ");
-        int y = in.nextInt();
-        System.out.println("y: ");
-        int x= in.nextInt();
-
-        TurtleGraphics turtleGraphics = new TurtleGraphics(width, height, x, y);
-
-        turtleGraphics.showBoard();
-
-        System.out.println(" If you print u+number of dots it moves up \n If you print d+number of dots it moves down \n If you print r+number of dots it moves right  \n If you print l+number of dots it down");
-        System.out.println("To clear the board print c");
-        System.out.println("please write the command");
-
-        String line = sc.nextLine();
-while(true){
-     String[] dir = line.trim().split(" ");
-
-          switch (dir[0]) {
-            case "u":
-                turtleGraphics.movePenUp(Integer.parseInt(dir[1]));
-                break;
-            case "d":
-                turtleGraphics.movePenDown(Integer.parseInt(dir[1]));
-                break;
-            case "l":
-                turtleGraphics.movePenLeft(Integer.parseInt(dir[1]));
-                break;
-            case "r":
-                turtleGraphics.movePenRight(Integer.parseInt(dir[1]));
-                break;
-              case "c":
-                  turtleGraphics.clearBoard();
-                  break;
-        }
-        turtleGraphics.showBoard();
-        System.out.print("Print command: ");
-        line = sc.nextLine();
-    }
-}}
->>>>>>> 8ca6767a27a070bf3c7f58104491afd5475db1a7
