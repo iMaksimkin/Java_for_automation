1293049119f4d04534eb12f38497c77606e2e4ee
iMaksimkin <int-8@yandex.ru>
2019-06-27 17:10:37 +0400

    Revert "Revert "collections""
    This reverts commit 0e5f32cf3d73f24ce983823443e8bb6af8d93efe.

---
+++ src/com/sperasoft/mood/javaforqa/Task5And6/GenericsAndCollections/Practice4/Person.java
@@ -0,0 +1,100 @@
+package com.sperasoft.mood.javaforqa.Task5And6.GenericsAndCollections.Practice4;
+
+
+import java.util.*;
+
+public class Person implements Comparable<Person> {
+    private String name;
+    private int age=18;
+
+    public Person() {
+
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public Person(String name, int age) {
+        this.name = name;
+        this.age = age;
+    }
+
+    @Override
+    public int compareTo(Person p) {
+
+        return ((Integer) getAge()).compareTo(p.getAge());
+    }
+
+
+    //(!)Implement a comparator for the Person class comparing Person first by name and if the name is equal by age.
+// it will be better ti use one compare to both
+// @Override
+//public int compareTo(Person p) {
+//
+//    return Comparator.comparing(Person::getName).thenComparingInt(Person::getAge).compare(this, p);
+//    }
+
+    public Person(String name) {
+        this.name = name;
+    }
+
+    @Override
+    public String toString() {
+        return name + " " + age;
+    }
+
+    public static List<Person> getUniqueList(List<Person> p1, List<Person> p2) {
+
+
+        SortedSet<Person> newList = new TreeSet<>(p1);
+        newList.addAll(p2);
+        SortedSet<Person> newList1 = new TreeSet<>(newList);
+        List<Person> resultList = new ArrayList<>(newList1);
+        return resultList;
+
+    }
+    public static boolean isEquals (List<Person> p1, List<Person> p2) {
+       if (p1.size() != p2.size() )
+                return false;
+        for (int i =0; i<p1.size(); i++){
+            if (p1.get(i).equals(p2.get(i)))
+                return false;
+        }
+
+        return true;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if(this == obj)
+            return true;
+        if(obj == null || obj.getClass() != this.getClass())
+            return false;
+        return this.getName().equals(((Person) obj).getName()) && ((Person) obj).getAge() == this.getAge();
+    }
+
+
+    static class NameComparator implements Comparator<Person> {
+        public NameComparator() {
+        }
+
+        public int compare(Person p1, Person p2) {
+            return p1.getName().compareTo(p2.getName());
+        }
+
+    }
+}
+
---
+++ src/com/sperasoft/mood/javaforqa/Task5And6/GenericsAndCollections/Practice4/PersonDemo.java
@@ -0,0 +1,71 @@
+package com.sperasoft.mood.javaforqa.Task5And6.GenericsAndCollections.Practice4;
+
+import java.util.*;
+
+import static com.sperasoft.mood.javaforqa.Task5And6.GenericsAndCollections.Practice4.Person.isEquals;
+
+public class PersonDemo {
+    public static void main(String[] args) {
+
+        List<Person> persons = new ArrayList<Person>();
+
+        persons.add(new Person("Dima"));
+        persons.add(new Person("Dasha", 113));
+        persons.add(new Person("Misha", 44));
+        persons.add(new Person("Shisha", 50));
+        persons.add(new Person("Elijah", 31));
+        persons.add(new Person("Alla", 64));
+        persons.add(new Person("Misha", 41));
+
+        System.out.println("sorted");
+
+        Collections.sort(persons);
+        for (Person p : persons) {
+            System.out.print(p.getName() + " " + p.getAge() + "\n");
+
+        }
+
+        System.out.println();
+        // The same method
+//
+
+   //     persons.sort(Person::compareTo);
+        Collections.sort(persons,new Person.NameComparator());
+        System.out.println("sort firstly by name and if the name is equal by age");
+        for (Person p : persons) {
+
+            System.out.print(p.getName() + " " + p.getAge() + "\n");
+
+        }
+        System.out.println();
+        Collections.shuffle(persons);
+        List<Person> wPersons = new ArrayList<>();
+        wPersons.add(new Person("Mrs.xz", 11));
+        wPersons.add(new Person("Dasha", 113));
+        wPersons.add(new Person("Shisha", 50));
+        wPersons.add(new Person("Erica", 20));
+        wPersons.add(new Person("Alla", 64));
+        System.out.println("Second list with woman names");
+        for (Person p : wPersons) {
+
+            System.out.print(p.getName() + " " + p.getAge() + "\n");
+
+        }
+        System.out.println();
+        System.out.println("Unique List ( without doubled woman names)");
+
+        for (Person p : Person.getUniqueList(persons,wPersons)) {
+            System.out.print(p.getName() + " " + p.getAge() + "\n");
+
+        }
+        System.out.println();
+        System.out.print("Is First equals Second: ");
+
+
+            System.out.print(isEquals(persons,wPersons));
+    }
+
+
+}
+
+
